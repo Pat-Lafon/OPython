6cebfc71587a4979b3d6770560e56f9ef14d51f8
wls63 <wls@cornell.edu>
2018-10-19 18:44:03 -0400

    Made progress evaluation without state

--- evaluate.ml
+++ evaluate.ml
@@ -1,8 +1,42 @@
 open Parser
 open State
 
+let helper_plus = function 
+  | (Int (x), Int(y)) -> Int(x+y)
+  | (String (x), String (y)) -> String (x ^ y)
+  | _ -> failwith "wrong types"
+
+let helper_minus = function 
+  | (Int (x), Int(y)) -> Int(x-y)
+  | _ -> failwith "wrong types"
+
+let helper_multiply = function 
+  | (Int (x), Int(y)) -> Int (x * y)
+  | _ -> failwith "wrong types"
+
+let helper_divide = function 
+  | (Int (x), Int(y)) -> Int(int_of_float (floor ((float_of_int x)/.(float_of_int y))))
+  | _ -> failwith "wrong types"
+
 let rec evaluate (exp : expr) : value = 
   match exp with 
-  | Binary (e1, op, e2) -> Int (0)
-  | Unary (op, e1) -> Int (0)
+  | Binary (e1, op, e2) -> begin 
+      match op with 
+      | Plus -> helper_plus (evaluate e1, evaluate e2)
+      | Minus -> helper_minus (evaluate e1, evaluate e2)
+      | Multiply -> helper_multiply (evaluate e1, evaluate e2)
+      | Divide -> helper_divide (evaluate e1, evaluate e2)
+    end
+  | Unary (op, e1) -> begin 
+      match (op,evaluate e1) with 
+      | (Plus, Int (x)) -> Int (x)
+      | (Minus, Int (x)) -> Int (-x)
+      | _ -> failwith "wrong types"
+    end
+  | Variable (x) -> Int (0)
   | Value (x) -> x
+
+(* let eval input st = match input with
+   | Some s, expr -> insert s (evaluate expr) st
+   | None, expr -> print (evaluate expr)
+*)
--- parser.mli
+++ parser.mli
@@ -1,7 +1,7 @@
 open State
 
 type op = Plus | Minus | Divide | Multiply
-type expr = Binary of (expr * op * expr) | Unary of (op * expr) | Value of State.value
+type expr = Binary of (expr * op * expr) | Unary of (op * expr) | Value of State.value | Variable of string
 
 val get_idx : string -> char -> int option
 
