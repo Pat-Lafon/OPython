d68309b5ff31fb13b5911552f399ab6da3d4908b
Zaibo Wang <zaibowang@gmail.com>
2018-10-19 19:07:19 -0400

    Fix parser building

--- parser.ml
+++ parser.ml
@@ -13,7 +13,7 @@ let reserved_keywords = [
   "with"; "assert";	"finally";	"nonlocal";	"yield"; "break";	"for"; "not"; 
   "class";	"from";	"or";	"continue";	"global";	"pass"]
 
-let is_var_name (s:string) : unit = 
+let is_var_name (s:string) : string = 
   (* Check that the first letter is not an integer*)
   let _ = let num = Char.code s.[0] in 
     if (48 <= num && num <= 57) 
@@ -25,7 +25,7 @@ let is_var_name (s:string) : unit =
                           || (97 <= num && num <= 122) || (num = 95) 
                        then x else raise VarNameInvalid) s in 
   (* Check that var is not a reserved keyword *)
-  if List.mem s reserved_keywords then raise ReservedVarName else ()
+  if List.mem s reserved_keywords then raise ReservedVarName else s
 
 let get_idx str char =
     try Some (String.index str char) with
@@ -36,16 +36,18 @@ let is_assignment line =
     | Some idx -> not ((String.get line (idx+1)) = '=')
     | None -> false
 
-let rec parse_expr_helper line curr len = 
-    if (String.length line) < curr + len then []
-    else (String.get (curr + len) )
+let rec parse_expr_helper str : expr = 
+    match get_idx str '+' with
+    | None -> Value(Int(int_of_string (String.trim str)))
+    | Some idx -> let left = String.trim (String.sub str 0 idx) in
+    let right = String.trim (String.sub str (idx + 1) ((String.length str) - idx)) in
+    Binary(parse_expr_helper left, Plus, parse_expr_helper right)
 
-
-let parse_expr line = parse_expr_helper line 0 1
+let parse_expr line = parse_expr_helper line
 
 let parse_assignment line = 
     let eq_idx = String.index line '=' in
-    let left = String.trim (String.sub line 0 eq_idx) in
+    let left = is_var_name (String.trim (String.sub line 0 eq_idx)) in
     let right = String.trim (String.sub line (eq_idx + 1) ((String.length line) - eq_idx)) in
     (Some left, parse_expr right)
 
--- parser.mli
+++ parser.mli
@@ -1,11 +1,13 @@
 open State
 
 type op = Plus | Minus | Divide | Multiply
-type expr = Binary of (expr * op * expr) | Unary of (op * expr) | Value of State.value
+type expr = Binary of (expr * op * expr) | Unary of (op * expr) | Value of State.value | Variable of string
 
 val get_idx : string -> char -> int option
 
 val is_assignment : string -> bool
+
+val parse_expr : string -> expr
 
 val parse_expr : string -> expr
 
