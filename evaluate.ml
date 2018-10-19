9b702ed2d6f6b69b147b8a7b46a72fea2d0f7923
esh63 <esh63@cornell.edu>
2018-10-19 18:44:34 -0400

    comm for merg

--- evaluate.ml
+++ evaluate.ml
@@ -3,6 +3,13 @@ open State
 
 let rec evaluate (exp : expr) : value = 
   match exp with 
-  | Binary (e1, op, e2) -> Int (0)
-  | Unary (op, e1) -> Int (0)
-  | Value (x) -> x
+  | Binary (e1, op, e2) -> begin
+      match op with 
+        Plus -> (evaluate e1) + (evaluate e2)
+                  Minus -> (evaluate e1) - (evaluate e2)
+                             Divide -> (evaluate e1) / (evalaute e2)
+                                         Multiply -> (evaluate e1) * (evaluate e2)
+
+
+      | Unary (op, e1) -> Int (0)
+      | Value (x) -> x
