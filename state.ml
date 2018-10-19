56e19c2b3ba36ab0760876ddd3c3439328582c07
esh63 <esh63@cornell.edu>
2018-10-19 17:37:50 -0400

    state is done

--- state.ml
+++ state.ml
@@ -1,2 +1,17 @@
-type value = Int of int| String of string
+
+type value = Int of int| String of string | Var of string
 type t = (string*value)list
+
+let empty : t = []
+
+let is_empty (d:t) : bool = (d = [])
+
+let rec insert (k:string) (v:value) (d:t) : t = 
+  if List.mem_assoc k d then insert k v (List.remove_assoc k d)
+  else (k,v)::d
+
+let find (k:string) (d:t) = 
+  try Some (List.assoc k d) with
+  | Not_found -> None
+
+let member (k:string) (d:t) : bool = List.mem_assoc k d
