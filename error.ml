95e51b939ae61be9824751d5428f0b41ffe8e6e0
Patrick LaFontaine <pwl45@cornell.edu>
2018-10-20 00:48:48 -0400

    errors and error handeling

--- error.ml
+++ error.ml
@@ -1,3 +1,6 @@
 exception SyntaxError of string
 exception TypeError of string
 exception NameError of string
+exception ZeroDivisionError of string
+exception OverflowError of string
+exception IndentationError of string
