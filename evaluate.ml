79b29e8099a0aec8fd836ec8dac25bb537866104
Patrick LaFontaine <pwl45@cornell.edu>
2018-10-19 20:49:59 -0400

    cleaned up eval

--- evaluate.ml
+++ evaluate.ml
@@ -18,31 +18,31 @@ let helper_divide = function
   | (Int (x), Int(y)) -> Int(int_of_float (floor ((float_of_int x)/.(float_of_int y))))
   | _ -> failwith "wrong types"
 
-let rec eval (exp : expr) (st : State.t) : value = 
-  match (exp,st) with 
-  | (Binary (e1, op, e2), st) -> begin 
-      match op with 
-      | Plus -> helper_plus (eval e1 st, eval e2 st)
-      | Minus -> helper_minus (eval e1 st, eval e2 st)
-      | Multiply -> helper_multiply (eval e1 st, eval e2 st)
-      | Divide -> helper_divide (eval e1 st, eval e2 st)
-    end
-  | Unary (op, e1), st-> begin 
-      match (op,eval e1 st) with 
-      | (Plus, Int (x)) -> Int (x)
-      | (Minus, Int (x)) -> Int (-x)
-      | _ -> failwith "wrong types"
-    end
-  | Variable (x),st -> begin 
-      match State.find x st with 
-      | Some t -> t
-      | None -> failwith "undefined types"
-    end
+let rec eval (exp : expr) (st : State.t) : value = match exp with 
+  | Binary (e1, op, e2) -> 
+    (match op with 
+     | Plus -> helper_plus (eval e1 st, eval e2 st)
+     | Minus -> helper_minus (eval e1 st, eval e2 st)
+     | Multiply -> helper_multiply (eval e1 st, eval e2 st)
+     | Divide -> helper_divide (eval e1 st, eval e2 st))
+  | Unary (op, e1) ->
+    (match (op, eval e1 st) with 
+     | (Plus, Int (x)) -> Int (x)
+     | (Minus, Int (x)) -> Int (-x)
+     | _ -> failwith "wrong types")
+  | Variable x -> 
+    (match State.find x st with 
+     | Some t -> t
+     | None -> failwith "undefined types")
+  | Value x -> x
 
-  | Value (x) ,st -> x
+let print (value:State.value):unit = 
+  (match value with
+   | Int x -> string_of_int x
+   | Float x -> string_of_float x
+   | Bool x -> string_of_bool x
+   | String x -> x) |> print_endline
 
 let evaluate input st = match input with
   | Some s, expr -> insert s (eval expr st) st
-  | None, expr -> (match (eval expr st) with 
-    | Int i -> print_endline (string_of_int i); st
-    | String s -> print_endline s; st)
+  | None, expr -> print (eval expr st); st
