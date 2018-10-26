c51010ebd698e8a6b1aa4beb06fd56624a4efb52
esh63 <esh63@cornell.edu>
2018-10-26 18:18:12 -0400

    append done

--- builtin.ml
+++ builtin.ml
@@ -2,7 +2,13 @@ open Parser
 open State
 open Evaluate
 
-let append (exp : expr) = 
-  match exp with
-  | Function (s,explist) -> 
-  | _ -> failwith("wrong data type")
+let append (explist : expr list) (st : State.t) = 
+  let vallist = List.map (fun x -> eval x st) explist in
+  match vallist with
+  | lst::valu::[] -> 
+    (match lst with
+     | VList x -> List.rev(valu::List.rev(x))
+     | _ -> failwith("not a list")
+    )
+  | _ -> failwith("not enough args")
+
