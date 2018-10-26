795fc226845ff5483277092566f4113a3ae65970
esh63 <esh63@cornell.edu>
2018-10-26 18:55:34 -0400

    started tests

--- builtin.mli
+++ builtin.mli
@@ -1,9 +1,9 @@
-open Parser
-open State
-open Evaluate
+(* open Parser
+   open State
+   open Evaluate
 
-val append : expr list -> State.t -> State.value
+   val append : expr list -> State.t -> State.value
 
-val length : expr list -> State.t -> State.value
+   val length : expr list -> State.t -> State.value
 
-val range : expr list -> State.t -> State.value
+   val range : expr list -> State.t -> State.value *)
--- test.ml
+++ test.ml
@@ -13,7 +13,24 @@ let make_line_test
 (**** TESTING *****)
 
 let line_tests = [
-  make_line_test "Plus" "x=4+5" ["x",Int(9)]
+  make_line_test "Plus" "x=4+5" ["x",Int(9)];
+  make_line_test "Subtract" "y=4-5" ["y",Int(-1)];
+  make_line_test "Div" "x=55/5" ["x",Float(11.)];
+  make_line_test "FloorDiv" "x=55//5" ["x",Int(11)];
+  make_line_test "Mult" "x=9*5" ["x",Int(45)];
+  make_line_test "Modular" "x=53%5" ["x",Int(3)];
+  make_line_test "Exp" "x=2**5" ["x",Int(32)];
+  make_line_test "Div" "x=55//5" ["x",Int(11)];
+  make_line_test "Equal" "x=(1==True)" ["x",Bool(true)];
+  make_line_test "notEqual" "x=(4!=3)" ["x",Bool(false)];
+  make_line_test "GreaterThan" "x=55>5" ["x",Bool(true)];
+  make_line_test "LessThan" "x=55<5" ["x",Bool(false)];
+  make_line_test "GreaterEqual" "x=5>=5" ["x",Bool(true)];
+  make_line_test "LessEqual" "x=5<=5" ["x",Bool(true)];
+  make_line_test "And" "x=True and True" ["x",Bool(true)];
+  make_line_test "Or" "x=False or True" ["x",Bool(true)];
+  make_line_test "Not" "x=(not False)" ["x",Bool(true)];
+  make_line_test "LessThan" "x=~5" ["x",Int(-6)];
 ]
 
 let suite =
