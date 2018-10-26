50b1f4897e9f77f38ddab8cca08db1481ba65aae
esh63 <esh63@cornell.edu>
2018-10-26 18:39:06 -0400

    line test

--- test.ml
+++ test.ml
@@ -1,5 +1,20 @@
 open OUnit2
 open Parser
+open Evaluate
+open State
+
+let make_line_test
+    (name : string)
+    (line : string)
+    (expected_output : State.t) : test = 
+  name >:: (fun _ -> 
+      assert_equal expected_output (evaluate (parse_line line) []))
+
+(**** TESTING *****)
+
+let line_tests = [
+  make_line_test "Plus" "x=4+5" ["x",Int(9)]
+]
 
 let suite =
   "test suite for A2"  >::: List.flatten [
