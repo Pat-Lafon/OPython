4fa29c3f93e8cacdeda8e117b30db103feff9e5b
Patrick LaFontaine <pwl45@cornell.edu>
2018-10-26 21:41:31 -0400

    commit for merge

--- main.ml
+++ main.ml
@@ -16,32 +16,32 @@ let rec read_while (cond : expr) (body : string) (lines : string list) =
   match lines with
   | [] -> print_string "... "; read_while cond body [read_line ()]
   | h::t -> (match parse_multiline h with
-            | Empty -> (cond, String.trim body)
-            | _ -> read_while cond (body ^ "\n" ^ String.trim h) t)
+      | Empty -> (cond, String.trim body)
+      | _ -> read_while cond (body ^ "\n" ^ String.trim h) t)
 
 let rec interpret (st:State.t) (lines: string list) : unit =
   match lines with
   | [] -> print_string ">>> "; interpret st [read_line ()]
   | h::t -> (match Parser.parse_line h |> (fun x -> Evaluate.evaluate x st) with
-    | exception (SyntaxError x) -> print_endline ("SyntaxError: "^x); interpret st []
-    | exception (NameError x) -> print_endline ("NameError: "^x); interpret st []
-    | exception (TypeError x) -> print_endline ("TypeError: "^x); interpret st []
-    | exception (OverflowError x) -> print_endline ("OverflowError: "^x); interpret st []
-    | exception (IndentationError x) -> print_endline ("IndentationError"^x); interpret st []
-    | exception (ZeroDivisionError x)-> print_endline ("ZeroDivisionError: "^x); interpret st []
-    | exception EmptyInput -> interpret st []
-    | exception (IfMultiline (cond, body)) -> 
-      (* Create list of conditions with corresponding line bodies *)
-      let (conds, bodies) = read_if [cond] [] body in interpret_if conds bodies st
-    | exception (WhileMultiline (cond, init_body)) -> 
-      (* Parse out the loop condition and body, process them in [interpret_while] *)
-      let (while_cond, while_body) = read_while cond (String.trim init_body) t in
-      let while_line = h in
-      interpret_while while_cond while_body while_line st
-    | newst -> interpret newst t)
+      | exception (SyntaxError x) -> print_endline ("SyntaxError: "^x); interpret st []
+      | exception (NameError x) -> print_endline ("NameError: "^x); interpret st []
+      | exception (TypeError x) -> print_endline ("TypeError: "^x); interpret st []
+      | exception (OverflowError x) -> print_endline ("OverflowError: "^x); interpret st []
+      | exception (IndentationError x) -> print_endline ("IndentationError"^x); interpret st []
+      | exception (ZeroDivisionError x)-> print_endline ("ZeroDivisionError: "^x); interpret st []
+      | exception EmptyInput -> interpret st []
+      | exception (IfMultiline (cond, body)) -> 
+        (* Create list of conditions with corresponding line bodies *)
+        let (conds, bodies) = read_if [cond] [] body in interpret_if conds bodies st
+      | exception (WhileMultiline (cond, init_body)) -> 
+        (* Parse out the loop condition and body, process them in [interpret_while] *)
+        let (while_cond, while_body) = read_while cond (String.trim init_body) t in
+        let while_line = h in
+        interpret_while while_cond while_body while_line st
+      | newst -> interpret newst t)
 and interpret_if (conds : expr list) (bodies : string list) (st: State.t) : unit =
   (* Go through [conds] and respective [bodies] in order. If any condition evaluates to true,
-  then we run the corresponding body through the interpreter and throw out the rest *)
+     then we run the corresponding body through the interpreter and throw out the rest *)
   match conds, bodies with
   | cond::c_t, body::b_t -> print_endline ("@" ^ body ^ "@"); (match Evaluate.eval cond st |> Evaluate.if_decider with
       | true -> interpret st (String.split_on_char '\n' body)
@@ -49,20 +49,13 @@ and interpret_if (conds : expr list) (bodies : string list) (st: State.t) : unit
   | _, _ -> raise (SyntaxError "Conditional statements and bodies mismatched")
 and interpret_while (cond : expr) (body : string) (while_line) (st: State.t) : unit = 
   match Evaluate.to_bool cond st with
-<<<<<<< HEAD
-  | true -> main st (String.split_on_char '\n' (body ^ "\n" ^ while_line ^ "\n" ^ body ^ "\n"))
-  | false -> main st []
-
-(* Execute the game engine. *)
-=======
   | true -> (* If while conditional is true, then we want to interpret the body, and after that,
-    interpret the loop condition until it's false *)
+               interpret the loop condition until it's false *)
     let split_body = String.split_on_char '\n' body in
     let new_lines = split_body @ [while_line] @ split_body @ [""] in
-     interpret st new_lines
+    interpret st new_lines
   | false -> interpret st []
 
->>>>>>> 18d5cd0647a1f605756f65ce5f30a93aac235dd6
 let _ = 
   ANSITerminal.(print_string [green]   "***A Python interpreter written in Ocaml***\n");
   ANSITerminal.(print_string [cyan]    "Authors: Patrick, Zaibo, William, and Eric!\n");
