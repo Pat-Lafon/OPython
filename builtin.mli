6626ba507b6528791d921e8a81f479897f19a8cc
wls63 <wls@cornell.edu>
2018-10-28 15:14:56 -0400

    Range built in done

--- builtin.mli
+++ builtin.mli
@@ -1,17 +1,13 @@
-(* open Parser
-   open State
-   open Evaluate
+open Parser
+open State
+open Evaluate
 
-   val append : expr list -> State.t -> State.value
+val append : expr list -> State.t -> State.value
 
-   val length : expr list -> State.t -> State.value
+val length : expr list -> State.t -> State.value
 
-<<<<<<< HEAD
 val range : expr list -> State.t -> State.value
 
 val built_in_function_names : string list
 
 val built_in_functions : (string * (expr list -> State.t -> State.value)) list
-=======
-   val range : expr list -> State.t -> State.value *)
->>>>>>> 795fc226845ff5483277092566f4113a3ae65970
