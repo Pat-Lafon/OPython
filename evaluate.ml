98a4f9108440f37bf8a0b4932d5551710b2412b5
wls63 <wls@cornell.edu>
2018-10-29 21:36:08 -0400

    Done splice and range functions

--- evaluate.ml
+++ evaluate.ml
@@ -266,8 +266,61 @@ let rec eval (exp : expr) (st : State.t) : value = match exp with
       (List.assoc f built_in_functions) lst st else 
     if (List.mem_assoc f st) then VList([]) (* TO DO *) else raise (NameError ("name '"^f^"' is not defined"))
 
+and index (lst : expr list) (st : State.t) : State.value  = let func = function
+    | Int x, Int y -> (x = y)
+    | Int x, Float y -> (float_of_int x = y)
+    | Int x, Bool y -> if y then (x=1) else (x=0)
+    | Float x, Int y -> (x = float_of_int y)
+    | Float x, Float y -> (x = y)
+    | Float x, Bool y -> if y then (x=1.0) else (x=0.0)
+    | Bool x, Int y -> if x then (y=1) else (y=0)
+    | Bool x, Float y -> if x then (y=1.0) else (y=0.0)
+    | Bool x, Bool y -> (x = y)
+    | String x, _ | _, String x -> false
+    | VList x, _ | _, VList x -> false
+    | Function (args1, body1), Function (args2, body2) -> (body1 = body2)
+    | _, _ -> false
+  in let idx value l = List.find (fun x -> func (x,value)) l 
+  in match List.map (fun x -> eval x st) lst with
+  | h::VList(t)::[] -> idx h t
+  | String(s1)::String(s)::[] -> 
+    let rec search sub str = if (String.length sub > String.length str) then 
+        String.length sub else 
+      if String.sub str 0 (String.length sub) = sub then
+        0 else (1 + search sub (String.sub str 1 (String.length sub)))
+    in if (search s1 s >= String.length s) then Int(-1) else Int(search s1 s)
+  | _ -> raise (TypeError ("Operation not supported"))
 
-
+and splice (lst : expr list) (st : State.t) : State.value = 
+  let rec helper lst x y z = if z = 0 then 
+      raise (ValueError "Third argument must not be zero") else 
+    if y >= List.length lst then helper lst x (List.length lst) z else 
+    if x >= y then [] else List.nth lst x :: helper lst (x+z) y z in
+  let decider x len = if x < - len then 0 else 
+    if x < 0 then x + len else if x > len then len else x in
+  let splice_helper lst x y z = 
+    helper lst (decider x (List.length lst)) (decider x (List.length lst)) (decider x (List.length lst)) in
+  match lst with
+  | h1::h2::h3::[] -> begin match (eval h1 st, eval h2 st, eval h3 st) with 
+      | String(s), String(""), String("") -> String(s)
+      | VList(l), String(""), String("") -> VList(l)
+      | VList(l), String(""), Int(x) -> VList(splice_helper l 0 x 1)
+      | VList(l), Int(x), String("") -> VList(splice_helper l x (List.length l) 1)
+      | VList(l), Int(x), Int(y) -> VList(splice_helper l x y 1)
+      | _ -> raise (TypeError ("Operation not supported"))
+    end
+  | h1::h2::h3::h4::[] -> begin match (eval h1 st,eval h2 st, eval h3 st, eval h4 st) with 
+      | VList(l), String(""), String(""), String("") -> VList(l)
+      | VList(l), String(""), String(""), Int(x) -> VList(splice_helper l 0 (List.length l) x)
+      | VList(l), String(""), Int(x), String("") -> VList(splice_helper l 0 x 1)
+      | VList(l), Int(x), String(""), String("") -> VList(splice_helper l x (List.length l) 1)
+      | VList(l), Int(x), Int(y), String("") -> VList(splice_helper l x y 1)
+      | VList(l), Int(x), String(""), Int(y) -> VList(splice_helper l x (List.length l) y)
+      | VList(l), String(""), Int(x), Int(y) -> VList(splice_helper l 0 x y)
+      | VList(l), Int(x), Int(y), Int(z) -> VList(splice_helper l x y z)
+      | _ -> raise (TypeError ("Operation not supported"))
+    end 
+  | _ -> raise (TypeError ("Operation not supported"))
 and append (explist : expr list) (st : State.t) = 
 
   let vallist = List.map (fun x -> eval x st) explist in
@@ -289,8 +342,10 @@ and len (lst : expr list) (st : State.t) : State.value = match lst with
 
 and helper_range s f i = if i = 0 then 
     raise (ValueError "Third argument must not be zero") else 
-  if s >= f then [] else 
-    Int(s) :: helper_range (s+i) f i 
+  if i > 0 then (if s >= f then [] else 
+                   Int(s) :: helper_range (s+i) f i) else 
+    (if s <= f then [] else Int(s) :: helper_range (s+i) f i)
+
 
 and range (lst : expr list) (st : State.t) : State.value = match lst with
   | h::[] -> begin match eval h st with 
@@ -307,6 +362,8 @@ and range (lst : expr list) (st : State.t) : State.value = match lst with
       | _ -> raise (TypeError ("Unsupported type for this operation"))
     end
   | _ -> raise (TypeError("Range takes at most three arguments"))
+
+
 
 and built_in_function_names = ["append"; "length"; "range"]
 
--- evaluate.mli
+++ evaluate.mli
@@ -1,6 +1,9 @@
 open Parser
 open State
 
+(** [evaluate input st] returns the new state with the expression part of 
+    input evaluated to a value and added to the state if input has a string. 
+    Otherwise, it prints out the value of the expression part of input **)
 val evaluate : string option * Parser.expr -> State.t -> State.t
 
 val eval : expr -> State.t -> State.value
