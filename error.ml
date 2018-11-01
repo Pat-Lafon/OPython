a2736313d358d75e8fed07c2612ed872c7ba093f
Patrick LaFontaine <pwl45@cornell.edu>
2018-10-31 23:32:15 -0400

    created error file for standard errors

---
+++ error.ml
@@ -0,0 +1,7 @@
+exception SyntaxError of string
+exception TypeError of string
+exception NameError of string
+exception ValueError of string
+exception OverflowError of string
+exception IndentationError of string
+exception ZeroDivisionError of string
