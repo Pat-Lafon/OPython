749005551b4bbba4278e465802ee8e46a3174594
Zaibo Wang <zaibowang@gmail.com>
2018-11-05 19:47:07 -0500

    merge sort

--- opython.ml
+++ opython.ml
@@ -13,12 +13,14 @@ let _ =
   else
     let file_name = Sys.argv.(1) in
     let rec file_helper (file:in_channel) (lines : string list) 
-        (line_nums : int list) (curr : int)= 
+        (line_nums : int list) (curr : int) = 
       (match Pervasives.input_line file with
        | exception End_of_file -> 
          Pervasives.close_in file; (List.rev lines, List.rev line_nums)
        | "" -> file_helper file lines line_nums (curr + 1)
-       | text -> if String.get (String.trim text) 0 = '#' 
+       | text -> if String.length (String.trim text) = 0
+         then file_helper file lines line_nums (curr + 1)
+         else if String.get (String.trim text) 0 = '#' 
          then file_helper file lines line_nums (curr + 1)
          else file_helper file (text::lines) (curr::line_nums) (curr + 1)) in
     let (lines, line_nums) = file_helper (Pervasives.open_in file_name) [] [] 1 in
