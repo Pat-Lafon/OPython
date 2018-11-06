e5303965ec63cba4553f2229901cf7331d4837b6
Zaibo Wang <zaibowang@gmail.com>
2018-11-05 20:51:20 -0500

    Add fucntion name test case

--- tests/function_name_test.py
+++ tests/function_name_test.py
@@ -9,3 +9,7 @@ a = xorx(1)
 a = xandx(1)
 a = xnotx(1)
 a = xdefx(1)
+
+def f(): return 5
+
+f()
--- tests/test_test.py
+++ tests/test_test.py
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-=======
-def d(arr):
-    arr[1] = 5
+def f(x):
+    for i in range(0, 5):
+        print(i)
 
-
-def f(arr):
-    l = [1, 2, 3]
-    d(l)
-    print(l)
-
-
-a = [1, 2, 3, 4]
-f(a)
-print(a)
->>>>>>> 81c6c6783c87b613d5d86b111c7c8853d0c1d78e
+f(5)
